90036df26eb21e67dad4fa260976f2a75100c926
jorenamc <jorenamc@amdocs.com>
2022-09-16 14:53:19 +0800

    removed some lines

--- src/com/training/assignment1/MyClass.java
+++ src/com/training/assignment1/MyClass.java
@@ -5,11 +5,11 @@ public class MyClass {
 
 
         //Assignment #1
-        com.training.assignment1.Student s1 = new com.training.assignment1.Student("Taylor", 21, 1, 34);
-        com.training.assignment1.Student s2 = new com.training.assignment1.Student("Ariana", 23, 2, 20);
-        com.training.assignment1.Student s3 = new com.training.assignment1.Student("Dua", 22, 3, 80);
-        com.training.assignment1.Student s4 = new com.training.assignment1.Student("Doja", 20, 4, 29);
-        com.training.assignment1.Student s5 = new com.training.assignment1.Student("Olivia", 21, 5, 45);
+        Student s1 = new Student("Taylor", 21, 1, 34);
+        Student s2 = new Student("Ariana", 23, 2, 20);
+        Student s3 = new Student("Dua", 22, 3, 80);
+        Student s4 = new Student("Doja", 20, 4, 29);
+        Student s5 = new Student("Olivia", 21, 5, 45);
 
 
         //Assignment #2
@@ -23,16 +23,16 @@ public class MyClass {
 
 
         //Assignment #3
-        com.training.assignment1.Employee e1 = new com.training.assignment1.Employee("Kate",3);
-        com.training.assignment1.Employee e2 = new com.training.assignment1.Employee("Harry",2);
-        com.training.assignment1.Employee e3 = new com.training.assignment1.Employee("Diana",1);
-        com.training.assignment1.Employee e4 = new com.training.assignment1.Employee("Charles",4);
-        com.training.assignment1.Employee e5 = new com.training.assignment1.Employee("Elizabeth",5);
+        Employee e1 = new Employee("Kate",3);
+        Employee e2 = new Employee("Harry",2);
+        Employee e3 = new Employee("Diana",1);
+        Employee e4 = new Employee("Charles",4);
+        Employee e5 = new Employee("Elizabeth",5);
 
-        com.training.assignment1.Employee[] arr = {e1,e2,e3,e4,e5};
+        Employee[] arr = {e1,e2,e3,e4,e5};
 
         //Sort the array in ascending order using two for loops
-        com.training.assignment1.Employee temp;
+        Employee temp;
         for (int i = 0; i < arr.length; i++) {
             for (int j = i+1; j <arr.length; j++) {
                 if(arr[i].years_exp >arr[j].years_exp) {      //swap elements if not in order
@@ -44,8 +44,8 @@ public class MyClass {
         }
 
         //Printing the results
-        for(com.training.assignment1.Employee emp : arr){
-            System.out.println("com.training.assignment1.Employee Name: " + emp.name + " | Years of Experience: " + emp.years_exp);
+        for(Employee emp : arr){
+            System.out.println("Employee Name: " + emp.name + " | Years of Experience: " + emp.years_exp);
         }
 
     }
