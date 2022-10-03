62ec3a29f5f0da08ad2a998835cdb53ac12d15ea
jorenamc <jorenamc@amdocs.com>
2022-10-03 18:13:05 +0800

    Oct. 3 - JDBC

---
+++ src/main/java/com/training/day13_assignment/JdbcDemo.java
@@ -0,0 +1,60 @@
+package com.training.day13_assignment;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import main.java.com.training.day13_assignment.model.Student;
+
+public class JdbcDemo {
+    public static void main(String[] args) {
+
+        try {
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+            String url = "jdbc:oracle:thin:@localhost:1521/orcl.iiht.tech";
+            String userName = "system";
+            String password = "system";
+
+            Connection con = DriverManager.getConnection(url,userName,password);
+
+            //Store more records in Student table using stored procedure
+            String query1 = "{call ADD_STUDENT(?,?,?,?)}";
+            CallableStatement cs = con.prepareCall(query1);
+            cs.setString(1,"ST104");
+            cs.setString(2,"LOUIS");
+            cs.setString(3,"LAGUNA");
+            cs.setString(4,"CS");
+            cs.execute();
+
+            //Fetch all records from student table
+            String query2 = "SELECT * FROM STUDENT";
+            cs = con.prepareCall(query2);
+            cs.execute();
+            ResultSet rs = cs.executeQuery();
+
+            ArrayList<Student> studArr = new ArrayList<Student>();
+
+            while(rs.next()){
+                Student stud = new Student();
+                stud.setStudId(rs.getString(1));
+                stud.setStudName(rs.getString(2));
+                stud.setStudAddress(rs.getString(3));
+                stud.setDepartments(rs.getString(4));
+                studArr.add(stud);
+            }
+
+            //Display the records in sorted manner (sorted by name)
+            Collections.sort(studArr);
+
+            for(Student s : studArr){
+                System.out.println("Student ID: " + s.getStudId());
+                System.out.println("Student Name: " + s.getStudName());
+                System.out.println("Student Address: " + s.getStudAddress());
+                System.out.println("Student Department: " + s.getDepartments());
+                System.out.println();
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/com/training/day13_assignment/model/Student.java
@@ -0,0 +1,51 @@
+package main.java.com.training.day13_assignment.model;
+
+public class Student implements Comparable<Student>{
+    private String studId;
+    private String studName;
+    private String studAddress;
+    private String departments;
+
+    public String getStudId() {
+        return studId;
+    }
+
+    public void setStudId(String studId) {
+        this.studId = studId;
+    }
+
+    public String getStudName() {
+        return studName;
+    }
+
+    public void setStudName(String studName) {
+        this.studName = studName;
+    }
+
+    public String getStudAddress() {
+        return studAddress;
+    }
+
+    public void setStudAddress(String studAddress) {
+        this.studAddress = studAddress;
+    }
+
+    public String getDepartments() {
+        return departments;
+    }
+
+    public void setDepartments(String departments) {
+        this.departments = departments;
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        if(studName.compareTo(o.getStudName()) > 0){
+            return 1;
+        }else if(studName.compareTo(o.getStudName()) < 0){
+            return -1;
+        }else{
+            return 0;
+        }
+    }
+}
